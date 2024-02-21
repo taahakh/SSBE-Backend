4fe85d6b47bf3f6ded42e1e95dbed1063d700eec
Ahnaf Khalique <ahnafkhalique@hotmail.com>
2024-02-21 21:09:58 +0000

    progress

---
+++ database_models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=True, nullable=False)
+    api_key = db.Column(db.String(36), unique=True, nullable=False)
--- main.py
+++ main.py
@@ -1,14 +1,89 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPTokenAuth
+from database_models import db, User
+import uuid
 # from other import summarise
 import servicemanager as svm
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.app = app;
+db.init_app(app);
+# db = SQLAlchemy(app)
+auth = HTTPTokenAuth(scheme="Bearer")
 api = Api(app)
+
+# with app.app_context():
+#     db.create_all()
+
 sm = svm.ServiceManager()
 
+def has_token_user(request):
+    if 'Authorization' not in request.headers:
+        return False
+
+    auth_header = request.headers['Authorization']
+    if not auth_header.startswith('Bearer '):
+        return False
+
+    key = auth_header.split(' ')[1]
+    user = User.query.filter_by(api_key=key).first()
+    return user is not None
+
+@auth.verify_token
+def verify_token(token):
+    print(token)
+    if not token:
+        return None
+    user = User.query.filter_by(api_key=token).first()
+    print(user)
+    return user
+
+
+class LoginResource(Resource):
+    def post(self):
+        print(User.query.all())
+        print(request.headers)
+        # print(has_token_user(request))
+        # if (has_token_user(request)):
+        #     print("Login success - APIKEY: ", request.authorization)
+        #     return jsonify({'message': 'Login/Connect Successful!', 'state' : 'GOOD'})
+        # else:
+        x = request.get_json()
+        print(x)
+        user = User.query.filter_by(username=x['username']).first()
+        
+        if user is None:
+            print("Login FAILED - User doesn't exist: ")
+            return jsonify({'message': 'User does not exist!', 'state' : 'BAD'})
+
+        print("Login success: ", user)
+        return jsonify({"message": "Login/Connect Successful!", "api_key" : user.api_key, 'state' : 'GOOD'})
+
+
+class SignupResource(Resource):
+    def post(self):
+        x = request.get_json()
+        print(x)
+        user = User.query.filter_by(username=x['username']).first()
+        if user is None:
+            key = str(uuid.uuid4())
+            user = User(username=x['username'], password=x['password'], api_key=key)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"message" : "Signup Successful!", "api_key" : key, 'state' : 'GOOD'})
+        else:
+            return jsonify({'message' : "User already exists!", 'state' : 'BAD'})
+
 class JsonFileResource(Resource):
+    @auth.login_required
     def get(self, filename=None):
+        # print(request.headers)
+        print(auth.current_user())
         if filename == "sum_customisation":
             sum_customisation = sm.get_sum_customisation('md.json')
             return {'data': sum_customisation}
@@ -46,6 +121,8 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 api.add_resource(ServiceManagerResource, '/servicemanager/<action>')
 api.add_resource(JsonFileResource, '/jsonfile', '/jsonfile/<filename>')
+api.add_resource(SignupResource, '/auth/signup');
+api.add_resource(LoginResource, '/auth/login');
 
 @app.route('/home')
 def index():
